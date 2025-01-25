38d4184110019b18462b178c21f48296a2338622
c143 <fritter@gmx.com>
2025-01-25 14:51:31 +0100

    ttt5

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -240,25 +240,25 @@ def _ensure_downloads_dir() -> pathlib.Path:
 
 def fetch(url:str, name:Optional[Union[pathlib.Path, str]]=None, subdir:Optional[str]=None, gunzip:bool=False,
           allow_caching=not getenv("DISABLE_HTTP_CACHE")) -> pathlib.Path:
-    if url.startswith(("/", ".")): return pathlib.Path(url)
-    name_part = name or hashlib.md5(url.encode('utf-8')).hexdigest()
-    _ensure_downloads_dir().mkdir(parents=True, exist_ok=True)
-    fp = (_ensure_downloads_dir() / (subdir if subdir is not None else "") / name_part + (f".gunzip" if gunzip else ""))
-    if not fp.is_file() or not allow_caching:
-        # Create directory if needed
-        _dir = fp.parent
-        if not _dir.exists(): _dir.mkdir(parents=True, exist_ok=True)
-        if fp.is_file(): fp.unlink()
-        with urllib.request.urlopen(url, timeout=10) as r:
-            assert r.status == 200, f"HTTP status code: {r.status}"
-            length = int(r.headers.get('content-length', 0)) if not gunzip else None
-            readfile = gzip.GzipFile(fileobj=r) if gunzip else r
-            progress_bar:tqdm = tqdm(total=length, unit='B', unit_scale=True, desc=f"{url}", disable=CI)
-            with tempfile.NamedTemporaryFile(dir=_dir, delete=False) as f:
-                while chunk := readfile.read(16384): progress_bar.update(f.write(chunk))
-            fp = pathlib.Path(f.name).replace(fp)
-        if length and os.path.getsize(fp) < length: raise RuntimeError(f"fetch size incomplete: {fp.size} < {length}")
-    return fp
+  if url.startswith(("/", ".")): return pathlib.Path(url)
+  name_part = name or hashlib.md5(url.encode('utf-8')).hexdigest()
+  _ensure_downloads_dir().mkdir(parents=True, exist_ok=True)
+  fp = (_ensure_downloads_dir() / (subdir if subdir is not None else "") / name_part + (".gunzip" if gunzip else ""))
+  if not fp.is_file() or not allow_caching:
+    _dir = fp.parent
+    if not _dir.exists(): _dir.mkdir(parents=True, exist_ok=True)
+    if fp.is_file(): fp.unlink()
+    with urllib.request.urlopen(url, timeout=10) as r:
+      assert r.status == 200, f"HTTP status code: {r.status}"
+      length = int(r.headers.get('content-length', 0)) if not gunzip else None
+      readfile = gzip.GzipFile(fileobj=r) if gunzip else r
+      progress_bar:tqdm = tqdm(total=length, unit='B', unit_scale=True, desc=f"{url}", disable=CI)
+      with tempfile.NamedTemporaryFile(dir=_dir, delete=False) as f:
+        while chunk := readfile.read(16384): progress_bar.update(f.write(chunk))
+      #fp = pathlib.Path(f.name).replace(fp) #TODO: required?
+      pathlib.Path(f.name).replace(fp)
+      if length and (file_size:=os.path.getsize(fp)) < length: raise RuntimeError(f"fetch size incomplete: {file_size} < {length}")
+  return fp
 
 # *** Exec helpers
 
